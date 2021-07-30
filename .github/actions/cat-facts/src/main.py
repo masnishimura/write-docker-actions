f314c30d9805c8d11de9f47970a07e4f481bd92c
masnishimura <70608104+masnishimura@users.noreply.github.com>
2021-07-30 15:27:48 -0700

    Update main.py
    Removed the '+1' from random selector

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -20,7 +20,7 @@ print(fact_list)
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
 def select_random_fact(fact_arr):
-    return fact_arr[random.randint(0, len(fact_list)+1)]
+    return fact_arr[random.randint(0, len(fact_list))]
 
 random_fact = select_random_fact(fact_list)
 
